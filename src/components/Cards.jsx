79ff3d51dca4a04ce21366e162bf2b864152e75e
Utkarshking <Utkarshsinghclash@gmail.com>
2022-06-24 14:05:45 +0530

    added all the cards in forward section and added dropdowns and navbar

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,49 +1,33 @@
 import React from 'react'
 import "./Cards.css"
 import '../styles/main.css';
-function Cards() {
+function Cards({ players }) {
     return (
         <div>
             <div class="container">
-                <div class="child-flex-item" id="child-flex-1">
-                    <div class="child-child-flex-item" id="child-child-flex-photo-cristiano"></div>
-                    <div class="child-child-flex-item" id="child-child-flex-name">Cristiano Ronaldo</div>
-                    <div class="child-child-flex-item" id="child-child-flex-button">
-                        <a href="https://www.premierleague.com/players/2522/Cristiano-Ronaldo/stats" target="_blank"
-                            rel="noopener noreferrer">
-                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                                Button
-                            </button>
-                        </a>
+                {players.map((player) =>
+                (
+                    <div class="child-flex-item" id="child-flex-1">
+                        <div class="child-child-flex-item" >
+                            <img className="photoresize"src={player.photo}/>
+                        </div>
+                        <div class="child-child-flex-item" id="child-child-flex-name">{player.name}</div>
+                        <div class="child-child-flex-item" id="child-child-flex-button">
+                            <a href="https://www.premierleague.com/players/2522/Cristiano-Ronaldo/stats" target="_blank"
+                                rel="noopener noreferrer">
+                                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                                    Button
+                                </button>
+                            </a>
+                        </div>
                     </div>
-                </div>
-                <div class="child-flex-item" id="child-flex-2">
-                    <div class="child-child-flex-item" id="child-child-flex-photo-messi"></div>
-                    <div class="child-child-flex-item" id="child-child-flex-name">Lionel Messi</div>
-                    <div class="child-child-flex-item" id="child-child-flex-button">
-                        <a href="https://www.ligue1.com/player/stats?id=lionel-andres-messi-cuccittini" target="_blank"
-                            rel="noopener noreferrer">
-                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                                Button
-                            </button>
-                        </a>
-                    </div>
-                </div>
-                <div class="child-flex-item" id="child-flex-3">
-                    <div class="child-child-flex-item" id="child-child-flex-photo-neymar"></div>
-                    <div class="child-child-flex-item" id="child-child-flex-name">Neymar Jr.</div>
-                    <div class="child-child-flex-item" id="child-child-flex-button">
-                        <a href="https://www.ligue1.com/player/stats?id=neymar-da-silva-santos-junior" target="_blank"
-                            rel="noopener noreferrer">
-                             <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                                Button
-                            </button>
-                        </a>
-                    </div>
-                </div>
+
+                )
+        )}
             </div>
         </div>
-    )
+
+    );
 }
 
 export default Cards
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 // import './src/index.css'
 // import "./public/index.html/"
 module.exports = {
-  content: [ "./src/App.js", "./src/components/Navbar.jsx", "./src/components/Cards.jsx"],
+  content: [ "./src/App.js", "./src/components/Navbar.jsx", "./src/components/Cards.jsx","./src/components/Dropdown.jsx"],
   theme: {
     extend: {},
   },
