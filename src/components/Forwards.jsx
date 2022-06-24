5b696ee0cd37c4c06ee89b49c1847a597170c40d
Utkarshking <Utkarshsinghclash@gmail.com>
2022-06-24 10:05:43 +0530

    starting files are added

---
+++ src/components/Forwards.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Cards from "./Cards"
+function Forwards() {
+  return (
+    <div>
+      <Cards/>
+    </div>
+  )
+}
+
+export default Forwards
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// import './src/index.css'
+// import "./public/index.html/"
+module.exports = {
+  content: [ "./src/App.js", "./src/components/Navbar.jsx", "./src/components/Cards.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+// "./src/App.js", "./src/components/Navbar.jsx", "./src/components/Cards.jsx"
